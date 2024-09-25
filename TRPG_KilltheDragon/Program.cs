b95c36de306d3eff911cd302c1fd33fb519c0898
TheTimeOfRecord <131391796+TheTimeOfRecord@users.noreply.github.com>
2024-09-25 22:21:09 +0900

    Init
    초기 세팅

---
+++ TRPG_KilltheDragon/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TRPG_KilltheDragon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
